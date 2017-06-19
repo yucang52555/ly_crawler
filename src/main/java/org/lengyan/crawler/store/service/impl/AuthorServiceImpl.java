79e0664dfb0e75044726ab57c3b963fc91e643a4
kangtiancheng <kangtiancheng001@163.com>
2017-06-19 18:24:38 +0800

    古诗文网数据抓取

---
+++ src/main/java/org/lengyan/crawler/store/service/impl/AuthorServiceImpl.java
@@ -0,0 +1,27 @@
+package org.lengyan.crawler.store.service.impl;
+
+import java.util.List;
+
+import org.lengyan.crawler.store.dao.GushiwenAuthorMapper;
+import org.lengyan.crawler.store.model.po.Author;
+import org.lengyan.crawler.store.service.IAuthorService;
+import org.lengyan.crawler.utils.CommonUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("authorService")
+public class AuthorServiceImpl implements IAuthorService{
+	
+	@Autowired
+	private GushiwenAuthorMapper userOrderMapper;
+	
+	public void saveAuthors(List<Author> gswAuthor) {
+		Integer size = CommonUtils.sizeOf(gswAuthor);
+		for (int i = 0; i < size; i++) {
+			Author author = gswAuthor.get(i);
+			Integer result = userOrderMapper.insertSelective(author);
+			System.out.println(result);
+		}
+	}
+	
+}
---
+++ src/main/java/org/lengyan/crawler/utils/CommonUtils.java
@@ -0,0 +1,32 @@
+package org.lengyan.crawler.utils;
+
+import java.util.List;
+
+/**
+ * 集合工具类
+ * @author oycw
+ *
+ */
+public class CommonUtils {
+	
+	/**
+	 * 获取集合的大小
+	 * @param
+	 * @return 
+	 * @变更记录 2016年10月17日 上午10:58:43  ktc
+	 */
+	public static int sizeOf(List<?> elements) {
+		return elements == null ? 0: elements.size();
+	}
+	
+	/**
+	 * 获取数组的大小
+	 * @param
+	 * @return 
+	 * @变更记录 2016年10月17日 上午10:58:43  ktc
+	 */
+	public static int lengthOf(Object[] elements) {
+		return elements == null ? 0: elements.length;
+	}
+	
+}
