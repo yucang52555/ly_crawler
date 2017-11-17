7569aa683f878a5be39d19ceed130aa284f31436
kangtiancheng <kangtiancheng001@163.com>
2017-11-17 16:05:32 +0800

    新浪微博用户主页

--- src/main/java/org/lengyan/crawler/entry/generate/sina/SinaUserGenerate.java
+++ src/main/java/org/lengyan/crawler/entry/generate/sina/SinaUserGenerate.java
@@ -1,19 +1,16 @@
 package org.lengyan.crawler.entry.generate.sina;
 
-import org.lengyan.crawler.generate.GenerateHTMLUtils;
-import org.lengyan.crawler.store.model.po.xmlpo.jianshu.JianshuArticle;
-import org.lengyan.crawler.store.model.po.xmlpo.sina.SinaUser;
-import org.lengyan.crawler.store.service.service.jianshu.IJianshuArticleService;
-import org.lengyan.crawler.store.service.service.sina.ISinaUserService;
-import org.lengyan.crawler.store.service.serviceimpl.jianshu.JianshuArticleServiceImpl;
-import org.lengyan.crawler.store.service.serviceimpl.sina.SinaUserServiceImpl;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+
+import org.lengyan.crawler.generate.GenerateHTMLUtils;
+import org.lengyan.crawler.store.model.po.xmlpo.sina.SinaUser;
+import org.lengyan.crawler.store.service.service.sina.ISinaUserService;
+import org.lengyan.crawler.store.service.serviceimpl.sina.SinaUserServiceImpl;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * 新浪用户采集页面
@@ -24,30 +21,34 @@ public class SinaUserGenerate {
 	
 	private static ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:init-data.xml");
 	
+	private static Integer pageSize = 2000;
+	
 	public static void main(String[] args) {
 		//保存到数据库
 		context.start();
 		ISinaUserService sinaUserService = (SinaUserServiceImpl)context.getBean("sinaUserService");
 		List<SinaUser> sinaUsers = sinaUserService.selectAllUserList();
-		
-		List<Map<String, Object>> params = new ArrayList<Map<String, Object>>();
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("charset", "<meta charset='UTF-8'>");
-		map.put("title", "新浪用户微博信息");
-		StringBuilder hrefBuilder = new StringBuilder();
-		
-		for (SinaUser sinaUser : sinaUsers) {
-			hrefBuilder.append("<a href='").append(sinaUser.getUserUrl()).append("'>")
-			.append(sinaUser.getUserName()).append("</a><br/>");
-		}
-		
-		map.put("content", hrefBuilder.toString());
-		params.add(map);
-		
-		Map<String, String> urls = GenerateHTMLUtils.generate("F:\\kingkang\\workspace\\ly_crawler\\file\\HtmlTemplate.html",
-				"F:\\kingkang\\workspace\\ly_crawler\\file\\html", params);
-		for (Entry<String, String> en : urls.entrySet()) {
-			System.out.println(en.getKey() + "-->" + en.getValue());
+		for (int i = 0; i <= sinaUsers.size() / pageSize; i++) {
+			List<Map<String, Object>> params = new ArrayList<Map<String, Object>>();
+			StringBuilder hrefBuilder = new StringBuilder();
+			for (int j = 0; j < pageSize; j++) {
+				if (i * pageSize + j < sinaUsers.size()) {
+					SinaUser sinaUser = sinaUsers.get(i * pageSize + j);
+					hrefBuilder.append("<a href='").append(sinaUser.getUserUrl()).append("'>")
+					.append(sinaUser.getUserName()).append("</a><br/>");
+				}
+			}
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("charset", "<meta charset='UTF-8'>");
+			map.put("title", "新浪用户微博信息" + i);
+			map.put("content", hrefBuilder.toString());
+			params.add(map);
+			
+			Map<String, String> urls = GenerateHTMLUtils.generate("E:\\workspace\\lengyan_git\\ly_crawler\\file\\HtmlTemplate.html",
+					"E:\\workspace\\lengyan_git\\ly_crawler\\file\\html\\sina", params);
+			for (Entry<String, String> en : urls.entrySet()) {
+				System.out.println(en.getKey() + "-->" + en.getValue());
+			}
 		}
 	}
 }
